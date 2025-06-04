a337fb4bca58ea5e4d4c3b32b1036c3eee3a6ab3
danlee0677 <danlee0677@gmail.com>
2025-06-04 17:09:16 +0900

    make blanks

--- src/models.ts
+++ src/models.ts
@@ -5,10 +5,7 @@ import { User } from './types';
 export const connection = createConnection(process.env.MONGODB_URI ?? "")
 
 export const userSchema = new Schema<User>({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  roles: { type: [String], required: true },
-  asset: { type: Number, required: true }
+  
 })
 
 export const RectangleModel = connection.model<User>('rectangle', userSchema)
--- src/types.ts
+++ src/types.ts
@@ -5,10 +5,7 @@ export type State = {
 }
 
 export const userZodSchema = z.object({
-  username: z.string(),
-  password: z.string(),
-  roles: z.array(z.string()),
-  asset: z.number()
+  
 })
 
 export type User = z.infer<typeof userZodSchema>
